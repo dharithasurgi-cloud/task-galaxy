3ac715dc4577842d67dd357489fac10a4a356195
Dhara <dharithasurgi-cloud@users.noreply.github.com>
2026-09-08 10:41:21 -0400

    Refresh latest app shell

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "task-galaxy-v2";
+const CACHE_NAME = "task-galaxy-v3";
 const APP_SHELL = ["./", "./index.html", "./manifest.json", "./icon.svg"];
 
 self.addEventListener("install", event => {
